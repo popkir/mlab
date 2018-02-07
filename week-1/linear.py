8723709772fa86fe95cee785cd9776065b3fd79c
Phillip Kravtsov <phillip@kravtsov.net>
2018-02-06 21:24:50 -0800

    added some things

--- week-1/linear.py
+++ week-1/linear.py
@@ -8,3 +8,15 @@ class linear():
     def get_dataset(self):
         pass
 
+    def predict(self, inputs):
+        pass
+
+    def fit_normal(self):
+        pass
+
+    def fit_SGD(self, batch_size):
+        pass
+
+    def fit_BGD(self):
+        pass
+
