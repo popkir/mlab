fa644410a621ff11a512ad92737a95301bb5f667
Phillip Kravtsov <phillip@kravtsov.net>
2018-02-06 20:45:03 -0800

    more

---
+++ week-1/linear.py
@@ -0,0 +1,7 @@
+class linear():
+    def __init__(self):
+        pass
+
+    def get_dataset(self):
+        pass
+
---
+++ week-1/logistic.py
@@ -0,0 +1,34 @@
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Logistic():
+
+    def __init__(self):
+        self.weights = #Numpy array of zeros
+        self.biases = #see above
+        pass
+
+    def logistic(self, z):
+        pass
+
+    def get_data(self):
+        url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+        names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+        self.dataset = pandas.read_csv(url, names=names)
+
+    def get_sample(self, size):
+        #Get some number of samples from the dataset, turn them into vectors you can feed into the equations. 
+        pass    
+
+    def fit_SGD(self, batch_size):
+        pass
+
+    def fit_BGD(self):
+        #Use all the data, don't split into minibatches
+        pass
+
+    def predict(self, inputs):
+        pass
+
+    def plot(self):
