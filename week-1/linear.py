369ea3b324b639973772c55924e20f74f718e594
Phillip Kravtsov <phillip@kravtsov.net>
2018-02-06 22:23:30 -0800

    added link-o

--- week-1/linear.py
+++ week-1/linear.py
@@ -6,6 +6,7 @@ class linear():
         pass
 
     def get_dataset(self):
+        #get ur csv from https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data
         pass
 
     def predict(self, inputs):
